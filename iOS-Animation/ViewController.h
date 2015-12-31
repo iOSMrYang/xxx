ce5a80888ab03eef706feac39b0a5bea9496d8cc
杨杰 <yangjie@solot.com>
2015-12-31 14:29:46 +0800

    Initial Commit

---
+++ iOS-Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-Animation
+//
+//  Created by 杨杰 on 15/12/31.
+//  Copyright © 2015年 Skill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS-Animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-Animation
+//
+//  Created by 杨杰 on 15/12/31.
+//  Copyright © 2015年 Skill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
